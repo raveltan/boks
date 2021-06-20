e4044543401c385828c79bf097c245377bdb0bc0
Ravel Tan <raveltanjaya@outlook.com>
2021-06-20 16:23:16 +0700

    modified boks to be able to run custom command

--- main.c
+++ main.c
@@ -12,30 +12,59 @@
 // execv
 #include <unistd.h>
 
+#include <string.h>
+
 /******
  *  Functions declaration  *
  ******/
 
-void run();
+void run(int argc, char **argv);
 // Function to be used with clone
 int startAction(void *args);
+// Program manual
+void manual();
+
+/******
+ *  Structs  *
+ ******/
+
+struct run_data {
+  int size;
+  char **args;
+};
 
 /******
  *  Entry Point  *
  ******/
 
-int main() {
-  // TODO: add argument checking and pass to run
-  run();
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    manual();
+    exit(0);
+  }
+  if (strcmp(argv[1], "run") || strcmp(argv[1], "r")) {
+    if (argc < 3) {
+      perror("Please speficy a command to run");
+      exit(1);
+    }
+    run(argc, argv);
+  }
   return 0;
 }
 
-void run() {
-  printf("Running %s\n", "/bin/bash");
+// Handle running command on container
+void run(int argc, char **argv) {
+  // Flags to be used when cloning the processs
   int namespaces = CLONE_NEWUTS;
-  // CLONE require CAP_SYS_ADMIN thus needs to be run as root.
+  // clone the process with the namepaces flags and run startAction inside of
+  // it. CLONE require CAP_SYS_ADMIN thus needs to be run as root.
+  struct run_data data;
+  data.args = argv;
+  data.size = argc;
+  void *args = (void *)&data;
+
   pid_t child_pid =
-      clone(startAction, malloc(4096) + 4096, SIGCHLD | namespaces, NULL);
+      clone(startAction, malloc(4096) + 4096, SIGCHLD | namespaces, args);
   if (child_pid == -1) {
     perror("Clone error");
     exit(1);
@@ -44,10 +73,16 @@ void run() {
 }
 
 int startAction(void *args) {
-  char *argv[] = {"/bin/bash", NULL};
-  int result = execv("/bin/bash", argv);
+  struct run_data *data = (struct run_data *)args;
+  char **execution_data = malloc((data->size - 2) * sizeof(char **));
+  memcpy(execution_data, data->args + 2, (data->size - 2) * sizeof(char **));
+  int result = execv(data->args[2], execution_data);
   if (result != 0) {
     perror("error");
   }
   exit(result);
+}
+
+void manual() {
+  // Add program manual here
 }
